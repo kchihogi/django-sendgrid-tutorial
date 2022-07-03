2aba6d74607ea91e891639b2e92931dd556cdab4
kchihogi <kchihogi@gmail.com>
2022-07-03 20:40:18 +0900

    test and initial data

---
+++ contact_form/initial_data/master_data.py
@@ -0,0 +1,24 @@
+"""This module deletes and inserts master data into the DB.
+"""
+from contact.models import Bcc, MailSetting
+
+def add_mail_setting():
+    # DB データを全削除
+    for record in MailSetting.objects.all():
+        record.delete()
+
+    # DBにデータを追加
+    setting = MailSetting()
+    setting.sender = 'example@hogehoge.com'
+    setting.save()
+
+def add_bcc():
+    # DB データを全削除
+    for record in Bcc.objects.all():
+        record.delete()
+
+    # DBにデータを追加
+    bcc1 = Bcc(email = 'example@hoghoge.com')
+    bcc1.save()
+    bcc2 = Bcc(email = 'example2@hoghoge.com')
+    bcc2.save()
---
+++ contact_form/initial_data/tests/master_data1.py
@@ -0,0 +1,4 @@
+from initial_data import master_data
+
+master_data.add_mail_setting()
+master_data.add_bcc()
