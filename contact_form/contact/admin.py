5e2e9904bed441fc7a7669f57581bc016ce0f125
kchihogi <kchihogi@gmail.com>
2022-07-03 20:38:50 +0900

    expand models

--- contact_form/contact/admin.py
+++ contact_form/contact/admin.py
@@ -1,4 +1,9 @@
+"""This module set up the admin page of django site.
+"""
 from django.contrib import admin
-from .models import Contact
+from .models import Bcc, Contact, Customer, MailSetting
 
+admin.site.register(Customer)
 admin.site.register(Contact)
+admin.site.register(Bcc)
+admin.site.register(MailSetting)
--- contact_form/contact/models.py
+++ contact_form/contact/models.py
@@ -1,11 +1,48 @@
-import email
-from email import message
+"""This module defines the structure of DB.
+"""
 from django.db import models
 
+class Customer(models.Model):
+    """The model of customer table.
+    """
+    name = models.CharField('お客様氏名', null=False, blank=False, max_length=100)
+    email = models.EmailField('お客様メールアドレス', null=False, blank=False, unique=True)
+    newsletter = models.BooleanField('メルマガ希望', null=False, blank=False, default=True)
+    valid = models.BooleanField('有効', null=False, blank= False, default=True)
+    reject = models.BooleanField('拒否', null=False, blank=False, default=False)
+    count = models.IntegerField('問い合わせ回数', null=False, blank=False, default=0)
+
+    class Meta:
+        indexes = [
+            models.Index(fields=['email'], name='email_idx'),
+        ]
+
+    def __str__(self):
+        return self.name
+
 class Contact(models.Model):
-    email = models.EmailField()
-    subject = models.CharField(max_length=256)
-    message = models.TextField()
+    """The model of contact table.
+    """
+    name = models.CharField('お客様氏名', null=False, blank=False, max_length=100)
+    email = models.EmailField('お客様メールアドレス', null=False, blank=False)
+    subject = models.CharField('件名', null=False, blank=False, max_length=256)
+    message = models.TextField('本文', null=False, blank=False)
+    time = models.DateTimeField('送信日時', null=False, blank=True)
 
     def __str__(self):
         return self.email
+
+class Bcc(models.Model):
+    """The model of BCC table.
+    """
+    email = models.EmailField('担当者メールアドレス', null=False, blank=False)
+    name = models.CharField('担当者氏名', null=True, blank=True, max_length=100)
+
+    def __str__(self):
+        return self.email
+
+class MailSetting(models.Model):
+    """The model of mail setting table.
+    """
+    sender = models.EmailField('送信メールアドレス', null=False, blank=False)
+    enable = models.BooleanField('有効', null=False, blank=False, default=True)
