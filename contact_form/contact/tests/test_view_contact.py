48d608e38163bb232f1fc87c0813845d891b41c3
k_chihogi <k_chihogi@ekty.co.jp>
2022-07-14 12:59:43 +0900

    update test

--- contact_form/contact/tests/test_view_contact.py
+++ contact_form/contact/tests/test_view_contact.py
@@ -3,11 +3,29 @@
 from django.test import TestCase
 from django.urls import reverse
 
+from initial_data import master_data
+
 class ContactViewTest(TestCase):
     """This class is an object to test the ContactView."""
 
-    def test_no_profile(self):
-        """If no profile resistered, the about page returns 404.
+    def test_no_main_setting_and_bcc(self):
+        """If no MailSetting and BCC resistered, the maintenace page is returned.
         """
+        response = self.client.get(reverse('contact:contact'))
+        self.assertContains(response=response, text='Maintenance')
+
+    def test_no_bcc(self):
+        """If no BCC resistered, the maintenace page is returned.
+        """
+        master_data.add_mail_setting()
+        master_data.add_mail_setting()
+        response = self.client.get(reverse('contact:contact'))
+        self.assertContains(response=response, text='Maintenance')
+
+    def test_no_main_setting(self):
+        """If no MailSetting resistered, the maintenace page is returned.
+        """
+        master_data.add_bcc()
+        master_data.add_bcc()
         response = self.client.get(reverse('contact:contact'))
         self.assertContains(response=response, text='Maintenance')
